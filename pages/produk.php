eb30511a905cbc8a887f04ba2fab08698551717a
servant26 <alirpl16@gmail.com>
2023-10-03 00:48:51 +0800

    memperbaiki title yang tidak sesuai

--- pages/produk.php
+++ pages/produk.php
@@ -50,7 +50,7 @@ $produk = [
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dunia Kode</title>
+  <title>Belajar PHP</title>
     <link rel="icon" href="../assets/gambar/icon.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
